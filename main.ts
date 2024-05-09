fd0db7df5fc8cbf6ac5a70ed47402979cbc7617d
shivangrawat30 <rawatshivang30@gmail.com>
2024-05-09 16:49:03 +0530

    uri changes
    Signed-off-by: shivangrawat30 <rawatshivang30@gmail.com>

--- main.ts
+++ main.ts
@@ -1,43 +1,86 @@
-import * as mpl from "@metaplex-foundation/mpl-token-metadata";
+// import * as mpl from "@metaplex-foundation/mpl-token-metadata";
 const fs = require("fs");
 import * as web3 from "@solana/web3.js";
-import * as anchor from '@project-serum/anchor';
-import { unstable_createMuiStrictModeTheme } from "@mui/material";
+import * as metaplex from "@metaplex-foundation/mpl-token-metadata";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  createSignerFromKeypair,
+  none,
+  signerIdentity,
+  some,
+  PublicKey,
+} from "@metaplex-foundation/umi";
+import {
+  fromWeb3JsKeypair,
+  fromWeb3JsPublicKey,
+} from "@metaplex-foundation/umi-web3js-adapters";
 
-function loadKeypair(filename: string){
-    const secret = JSON.parse(fs.readFileSync(filename).toString()) as number[]
-    const secretKey = Uint8Array.from(secret)
-    return web3.Keypair.fromSecretKey(secretKey)
+function loadKeypair(filename: string) {
+  const secret = JSON.parse(fs.readFileSync(filename).toString()) as number[];
+  const secretKey = Uint8Array.from(secret);
+  return web3.Keypair.fromSecretKey(secretKey);
 }
+const INITIALIZE = false;
 async function main() {
-    console.log("let's name some tokens!");
-    const myKeypair = loadKeypair("shQ4ufEV8QQk379TmJ5siE7Vx264DWm5cKo4AZNWrpP.json");
-    const mintToken = new web3.PublicKey("SCiZSvd75wmyBbeK8GTAHrhSesLrXyzjQpn6BnVqHuD.json");
-    const seed1 = Buffer.from(anchor.utils.bytes.utf8.encode("metadata"));
-    const seed2 = Buffer.from(mpl.MPL_TOKEN_METADATA_PROGRAM_ID.toBytes());
-    const seed3 = Buffer.from(mintToken.toBytes());
-    const [metadataPDA, _pda] = web3.PublicKey.findProgramAddressSync([seed1,seed2,seed3], mpl.MPL_TOKEN_METADATA_PROGRAM_ID);
-    console.log(myKeypair.publicKey.toBase58());
+  console.log("let's name some tokens!");
+  const connection = new web3.Connection(
+    web3.clusterApiUrl("devnet"),
+    "confirmed"
+  );
+  const myKeypair = loadKeypair(
+    "shQ4ufEV8QQk379TmJ5siE7Vx264DWm5cKo4AZNWrpP.json"
+  );
+  const mint = new web3.PublicKey(
+    "SDCiUsUrxAjY5fH3QPesyMGQ4u2dBidQZMm6uwTR7Xm"
+  );
 
-    const accounts = {
-        metaData: metadataPDA,
-        mintToken,
-        mintAuthority: myKeypair.publicKey,
-        payer: myKeypair.publicKey,
-        updateAuthority: myKeypair.publicKey,
-    }
+  const umi = createUmi("https://api.devnet.solana.com");
+  const signer = createSignerFromKeypair(umi, fromWeb3JsKeypair(myKeypair));
+  umi.use(signerIdentity(signer, true));
 
-    const dataV2 = {
-        name: "ShivangCoin",
-        symbol: "SHC",
-        uri: ""
-    }
+  const ourMetadata = {
+      name: "Shivang Dev Coin",
+      symbol: "SDC",
+      uri: "https://raw.githubusercontent.com/ShivangRawat30/ShivangDevCoin/blob/main/SDC.jpg",
+    };
+    const onChainData = {
+        ...ourMetadata,
+        sellerFeeBasisPoints: 0,
+        creators: none<metaplex.Creator[]>(),
+        collection: none<metaplex.Collection>(),
+        uses: none<metaplex.Uses>(),
+    };
     
-    // mpl.createMetadataAccountV3
-    const {token, mint } = await mpl.createAndMint(
-        umi,
+    
+    if(INITIALIZE){
+        const account: metaplex.CreateMetadataAccountV3InstructionAccounts = {
+            mint: fromWeb3JsPublicKey(mint),
+            mintAuthority: signer,
+        };
+        const data: metaplex.CreateMetadataAccountV3InstructionDataArgs = {
+            isMutable: true,
+            collectionDetails: null,
+            data: onChainData,
+        };
+        const txid = await metaplex.createMetadataAccountV3(umi, {...account, ...data}).sendAndConfirm(umi);
+        console.log(txid);
+    }
+  else{
+    const data: metaplex.UpdateMetadataAccountV2InstructionData = {
+        data: some(onChainData),
+        discriminator: 0,
+        isMutable: some(true),
+        newUpdateAuthority: none<PublicKey>(),
+        primarySaleHappened: none<boolean>(),
+    }
+    const accounts: metaplex.UpdateMetadataAccountV2InstructionAccounts = {
+        metadata: metaplex.findMetadataPda(umi,{mint: fromWeb3JsPublicKey(mint)}),
+        updateAuthority: signer
+    }
+    const txid = await metaplex.updateMetadataAccountV2(umi, {...accounts, ...data} ).sendAndConfirm(umi);
+    console.log(txid)
 
-    )
+  }
 }
 
 main();
